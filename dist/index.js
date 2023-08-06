920cc469d6eaacacacb871e895e554830c00e7bf
takatoshi.tenko <chanten811@gmail.com>
2023-08-06 22:59:13 +0900

    p62 2.5.2　ブロック

--- dist/index.js
+++ dist/index.js
@@ -6,16 +6,20 @@ let greeting, target;
 greeting = "HEllo";
 target = "wotlf";
 console.log(greeting + target);
-import { createInterface } from "readline";
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-rl.question('文字列を入力してください\n', (line) => {
-    console.log(`${line}が入力されました`);
-    rl.close();
-});
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// rl.question('文字列を入力してください\n', (line) => {
+//   console.log(`${line}が入力されました`)
+//   rl.close()
+// })
 let userName = "";
 if (userName === "")
     userName = "名無し";
 console.log(userName);
+if (userName === "") {
+    console.log("名前を入力してください");
+    userName = "名無し";
+}
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,31 @@
-const message: string = "Hello World!"
-console.log(message)
+const message: string = "Hello World!";
+console.log(message);
 
-const test: string = "this is test."
-console.log(test)
+const test: string = "this is test.";
+console.log(test);
 
-let greeting,target
-greeting = "HEllo"
-target = "wotlf"
-console.log(greeting + target)
+let greeting, target;
+greeting = "HEllo";
+target = "wotlf";
+console.log(greeting + target);
 
-import { createInterface } from "readline"
+import { createInterface } from "readline";
 
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-rl.question('文字列を入力してください\n', (line) => {
-  console.log(`${line}が入力されました`)
-  rl.close()
-})
+// rl.question('文字列を入力してください\n', (line) => {
+//   console.log(`${line}が入力されました`)
+//   rl.close()
+// })
 
-let userName = ""
-if (userName === "") userName = "名無し"
-console.log(userName)
+let userName = "";
+if (userName === "") userName = "名無し";
+console.log(userName);
+
+if (userName === "") {
+  console.log("名前を入力してください");
+  userName = "名無し";
+}
