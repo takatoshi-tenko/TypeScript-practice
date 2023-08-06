a3f7e313b1957f523f45e64783dd5b65a845e5b2
takatoshi.tenko <chanten811@gmail.com>
2023-08-06 23:20:47 +0900

    p71 2.6.2 FizzBuzz問題

--- src/index.ts
+++ src/index.ts
@@ -85,8 +85,20 @@ let userName = "";
 //   console.log(i);
 // }
 
-let sum = 0;
-for (let i: number = 1; i <= 100; i++) {
-  sum += i;
+// let sum = 0;
+// for (let i: number = 1; i <= 100; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+for (let i = 0; i <= 100; i++) { 
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
-console.log(sum);
