508b8f1c75a4dc3ffa6d39be15c983d49607cb7c
takatoshi.tenko <chanten811@gmail.com>
2023-08-06 23:08:26 +0900

    p64 2.5.4 switch文

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,3 @@
-"use strict";
 // const message: string = "Hello World!";
 // console.log(message);
 // const test: string = "this is test.";
@@ -7,15 +6,29 @@
 // greeting = "HEllo";
 // target = "wotlf";
 // console.log(greeting + target);
-// import { createInterface } from "readline";
-// const rl = createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+import { createInterface } from "readline";
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 // rl.question('文字列を入力してください\n', (line) => {
 //   console.log(`${line}が入力されました`)
 //   rl.close()
 // })
+rl.question("コマンドを入力して下さい", (name) => {
+    switch (name) {
+        case "greet":
+            console.log("こんにちは。");
+            break;
+        case "cat":
+            console.log("あなたは猫派ですか？");
+            console.log("私は犬派です。");
+            break;
+        default:
+            console.log(`コマンド[${name}]を認識できませんでした。`);
+    }
+    rl.close();
+});
 let userName = "";
 // if (userName === "") userName = "名無し";
 // console.log(userName);
