033dff395b9142525edd8b4dde9aa88e3dbd9373
takatoshi.tenko <chanten811@gmail.com>
2023-08-06 23:11:12 +0900

    p66 2.5.5 while 文

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 // const message: string = "Hello World!";
 // console.log(message);
 // const test: string = "this is test.";
@@ -6,29 +7,29 @@
 // greeting = "HEllo";
 // target = "wotlf";
 // console.log(greeting + target);
-import { createInterface } from "readline";
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+// import { createInterface } from "readline";
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 // rl.question('文字列を入力してください\n', (line) => {
 //   console.log(`${line}が入力されました`)
 //   rl.close()
 // })
-rl.question("コマンドを入力して下さい", (name) => {
-    switch (name) {
-        case "greet":
-            console.log("こんにちは。");
-            break;
-        case "cat":
-            console.log("あなたは猫派ですか？");
-            console.log("私は犬派です。");
-            break;
-        default:
-            console.log(`コマンド[${name}]を認識できませんでした。`);
-    }
-    rl.close();
-});
+// rl.question("コマンドを入力して下さい", (name) => {
+//   switch (name) {
+//     case "greet":
+//       console.log("こんにちは。");
+//       break;
+//     case "cat":
+//       console.log("あなたは猫派ですか？");
+//       console.log("私は犬派です。");
+//       break;
+//     default:
+//       console.log(`コマンド[${name}]を認識できませんでした。`);
+//   }
+//   rl.close();
+// });
 let userName = "";
 // if (userName === "") userName = "名無し";
 // console.log(userName);
@@ -36,17 +37,22 @@ let userName = "";
 //   console.log("名前を入力してください");
 //   userName = "名無し";
 // }
-if (userName !== "") {
-    console.log("ちゃんと名前があってえらい！");
+// if (userName !== "") {
+//   console.log("ちゃんと名前があってえらい！");
+// } else {
+//   console.log("名前を入力してください");
+//   userName = "名無し";
+// }
+// let num = 0;
+// if (num < 0) {
+//   console.log("numは負の数です。");
+// } else if (num === 0) {
+//   console.log("numは0です。");
+// }
+let sum = 0;
+let i = 1;
+while (i <= 100) {
+    sum += i;
+    i++;
 }
-else {
-    console.log("名前を入力してください");
-    userName = "名無し";
-}
-let num = 0;
-if (num < 0) {
-    console.log("numは負の数です。");
-}
-else if (num === 0) {
-    console.log("numは0です。");
-}
+console.log(sum);
--- src/index.ts
+++ src/index.ts
@@ -9,32 +9,32 @@
 // target = "wotlf";
 // console.log(greeting + target);
 
-import { createInterface } from "readline";
+// import { createInterface } from "readline";
 
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
 // rl.question('文字列を入力してください\n', (line) => {
 //   console.log(`${line}が入力されました`)
 //   rl.close()
 // })
 
-rl.question("コマンドを入力して下さい", (name) => {
-  switch (name) {
-    case "greet":
-      console.log("こんにちは。");
-      break;
-    case "cat":
-      console.log("あなたは猫派ですか？");
-      console.log("私は犬派です。");
-      break;
-    default:
-      console.log(`コマンド[${name}]を認識できませんでした。`);
-  }
-  rl.close();
-});
+// rl.question("コマンドを入力して下さい", (name) => {
+//   switch (name) {
+//     case "greet":
+//       console.log("こんにちは。");
+//       break;
+//     case "cat":
+//       console.log("あなたは猫派ですか？");
+//       console.log("私は犬派です。");
+//       break;
+//     default:
+//       console.log(`コマンド[${name}]を認識できませんでした。`);
+//   }
+//   rl.close();
+// });
 
 let userName = "";
 // if (userName === "") userName = "名無し";
@@ -45,17 +45,25 @@ let userName = "";
 //   userName = "名無し";
 // }
 
-if (userName !== "") {
-  console.log("ちゃんと名前があってえらい！");
-} else {
-  console.log("名前を入力してください");
-  userName = "名無し";
-}
+// if (userName !== "") {
+//   console.log("ちゃんと名前があってえらい！");
+// } else {
+//   console.log("名前を入力してください");
+//   userName = "名無し";
+// }
 
-let num = 0;
+// let num = 0;
 
-if (num < 0) {
-  console.log("numは負の数です。");
-} else if (num === 0) {
-  console.log("numは0です。");
+// if (num < 0) {
+//   console.log("numは負の数です。");
+// } else if (num === 0) {
+//   console.log("numは0です。");
+// }
+
+let sum = 0
+let i = 1
+while(i <= 100) {
+  sum += i
+  i++
 }
+console.log(sum)
