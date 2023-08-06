1048154b67472d7b06554c266e0693fa573526f5
takatoshi.tenko <chanten811@gmail.com>
2023-08-06 23:04:48 +0900

    p63 2.5.3　負の数

--- dist/index.js
+++ dist/index.js
@@ -30,3 +30,10 @@ else {
     console.log("名前を入力してください");
     userName = "名無し";
 }
+let num = 0;
+if (num < 0) {
+    console.log("numは負の数です。");
+}
+else if (num === 0) {
+    console.log("numは0です。");
+}
--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,11 @@ if (userName !== "") {
   console.log("名前を入力してください");
   userName = "名無し";
 }
+
+let num = 0
+
+if (num < 0) {
+  console.log("numは負の数です。")
+} else if (num === 0) {
+  console.log("numは0です。")
+}
