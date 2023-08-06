6bef18ae8e9fd2830be63e0eaccbe051c362434a
takatoshi.tenko <chanten811@gmail.com>
2023-08-06 21:41:20 +0900

    package.jsonの更新

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,17 @@
-"use strict";
 const message = "Hello World!";
 console.log(message);
+const test = "this is test.";
+console.log(test);
+let greeting, target;
+greeting = "HEllo";
+target = "wotlf";
+console.log(greeting + target);
+import { createInterface } from "readline";
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question('文字列を入力してください\n', (line) => {
+    console.log(`${line}が入力されました`);
+    rl.close();
+});
