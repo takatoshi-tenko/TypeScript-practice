45352967b5f0179a0ddf79ebae258ab40b2df859
takatoshi.tenko <chanten811@gmail.com>
2023-08-07 23:04:38 +0900

    p100 3.4.2 型引数を持つ型を使用する

--- src/index.ts
+++ src/index.ts
@@ -213,3 +213,31 @@
 // console.log(data);
 // console.log(data.chicken);
 // console.log(data.弁当);
+
+// type FooBar = {
+//   foo: string;
+//   bar: number;
+// };
+// type FooBarBaz = {
+//   foo: string;
+//   bar: number;
+//   baz: boolean;
+// };
+// const obj: FooBarBaz = {
+//   foo: "hi",
+//   bar: 1,
+//   baz: false,
+// };
+// const obj2: FooBar = obj;
+// console.log(obj)
+// console.log(obj2)
+type Family<Parent, Child> = {
+  mother: Parent;
+  father: Parent;
+  child: Child;
+};
+const obj: Family<number, string> = {
+  mother: 0,
+  father: 100,
+  child: "1000",
+};
