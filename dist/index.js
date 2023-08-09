0754c94495d54a2db976769f1ca8fb3414cb5e5e
takatoshi.tenko <chanten811@gmail.com>
2023-08-09 21:47:52 +0900

    p141 4.1.4 アロー関数式で関数を作る

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,8 @@
 "use strict";
 // const message: string = "Hello World!";
 // console.log(message);
-const calcBMI = function (human) {
-    return human.weight / human.height ** 2;
-};
-const calcBMI2 = function ({ height, weight }) {
+const calcBMI = ({ height, weight }) => {
     return weight / height ** 2;
 };
-const uhyo = {
-    height: 1.84,
-    weight: 72,
-};
+const uhyo = { height: 1.84, weight: 72 };
 console.log(calcBMI(uhyo));
-console.log(calcBMI2(uhyo));
--- src/index.ts
+++ src/index.ts
@@ -408,23 +408,35 @@
 // helloWorldTimes2(5);
 // helloWorldTimes2(150);
 
+// type Human = {
+//   height: number;
+//   weight: number;
+// };
+
+// const calcBMI = function (human: Human): number {
+//   return human.weight / human.height ** 2;
+// };
+
+// // 分割代入を使用することもできる
+// const calcBMI2 = function ({ height, weight }: Human): number {
+//   return weight / height ** 2;
+// };
+
+// const uhyo: Human = {
+//   height: 1.84,
+//   weight: 72,
+// };
+// console.log(calcBMI(uhyo));
+// console.log(calcBMI2(uhyo));
+
 type Human = {
   height: number;
   weight: number;
 };
 
-const calcBMI = function (human: Human): number {
-  return human.weight / human.height ** 2;
-};
-
-// 分割代入を使用することもできる
-const calcBMI2 = function ({ height, weight }: Human): number {
+const calcBMI = ({ height, weight }: Human): number => {
   return weight / height ** 2;
 };
 
-const uhyo: Human = {
-  height: 1.84,
-  weight: 72,
-};
+const uhyo: Human = { height: 1.84, weight: 72 };
 console.log(calcBMI(uhyo));
-console.log(calcBMI2(uhyo));
