0081a0fd7e684cdcfe0aed30d5a10df38524adb2
takatoshi.tenko <chanten811@gmail.com>
2023-08-09 21:32:10 +0900

    p138 4.1.1 関数宣言

--- dist/index.js
+++ dist/index.js
@@ -8,31 +8,38 @@ Mary Sue,14,1
 `;
 // const result = data.trim().split(',')
 // console.log(result)
-const users = [];
-const lines = data.split('\n');
-for (const line of lines) {
-    if (line === "") {
-        continue; //スキップ処理
+// const users: User[] = []
+// const lines = data.split('\n')
+// for (const line of lines) {
+//   if(line === "") {
+//     continue //スキップ処理
+//   }
+//   const [name, ageString, premiumString] = line.split(',')
+//   console.log(name)
+//   console.log(ageString)
+//   console.log(premiumString)
+//   const age = Number(ageString)
+//   const premiumUser = premiumString === "1"
+//   console.log(premiumUser)
+//   users.push({
+//     name,
+//     age,
+//     premiumUser
+//   })
+// }
+// console.log(lines)
+// for (const user of users) {
+//   if (user.premiumUser) {
+//     console.log(`${user.name}(${user.age})はプレミアムユーザーです。`);
+//   } else {
+//     console.log(`${user.name}(${user.age})はプレミアムユーザーではありません。`);
+//   }
+// }
+function range(min, max) {
+    const result = [];
+    for (let i = min; i <= max; i++) {
+        result.push(i);
     }
-    const [name, ageString, premiumString] = line.split(',');
-    console.log(name);
-    console.log(ageString);
-    console.log(premiumString);
-    const age = Number(ageString);
-    const premiumUser = premiumString === "1";
-    console.log(premiumUser);
-    users.push({
-        name,
-        age,
-        premiumUser
-    });
+    return result;
 }
-console.log(lines);
-for (const user of users) {
-    if (user.premiumUser) {
-        console.log(`${user.name}(${user.age})はプレミアムユーザーです。`);
-    }
-    else {
-        console.log(`${user.name}(${user.age})はプレミアムユーザーではありません。`);
-    }
-}
+console.log(range(5, 10));
--- src/index.ts
+++ src/index.ts
@@ -351,32 +351,40 @@ Mary Sue,14,1
 // const result = data.trim().split(',')
 // console.log(result)
 
-const users: User[] = []
-const lines = data.split('\n')
-for (const line of lines) {
-  if(line === "") {
-    continue //スキップ処理
-  }
-  const [name, ageString, premiumString] = line.split(',')
-  console.log(name)
-  console.log(ageString)
-  console.log(premiumString)
-  const age = Number(ageString)
-  const premiumUser = premiumString === "1"
-  console.log(premiumUser)
-  users.push({
-    name,
-    age,
-    premiumUser
-  })
-}
-console.log(lines)
+// const users: User[] = []
+// const lines = data.split('\n')
+// for (const line of lines) {
+//   if(line === "") {
+//     continue //スキップ処理
+//   }
+//   const [name, ageString, premiumString] = line.split(',')
+//   console.log(name)
+//   console.log(ageString)
+//   console.log(premiumString)
+//   const age = Number(ageString)
+//   const premiumUser = premiumString === "1"
+//   console.log(premiumUser)
+//   users.push({
+//     name,
+//     age,
+//     premiumUser
+//   })
+// }
+// console.log(lines)
 
-for (const user of users) {
-  if (user.premiumUser) {
-    console.log(`${user.name}(${user.age})はプレミアムユーザーです。`);
-  } else {
-    console.log(`${user.name}(${user.age})はプレミアムユーザーではありません。`);
-  }
-}
+// for (const user of users) {
+//   if (user.premiumUser) {
+//     console.log(`${user.name}(${user.age})はプレミアムユーザーです。`);
+//   } else {
+//     console.log(`${user.name}(${user.age})はプレミアムユーザーではありません。`);
+//   }
+// }
 
+function range(min: number, max: number): number[] {
+  const result = []
+  for (let i = min; i <= max; i++) {
+    result.push(i)
+  }
+  return result
+}
+console.log(range(5, 10))
