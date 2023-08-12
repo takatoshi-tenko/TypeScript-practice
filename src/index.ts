a1949b421f81b6e51e2ba89b993a707cc32b930c
takatoshi.tenko <chanten811@gmail.com>
2023-08-12 12:46:52 +0900

    p220 5.3.5 implementsキーワードによるクラスの型チェック

--- src/index.ts
+++ src/index.ts
@@ -883,37 +883,73 @@ import { stringify } from "querystring";
 // };
 // console.log(john instanceof User);
 
-class User {
+// class User {
+//   name: string;
+//   #age: number;
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.#age = age;
+//   }
+//   public isAdult(): boolean {
+//     return this.#age >= 20;
+//   }
+// }
+// class PremiumUser extends User {
+//   rank: number = 1;
+//   public isAdult(): boolean {
+//     return true;
+//   }
+// }
+// const uhyo = new PremiumUser("uhyo", 26);
+// const taro = new PremiumUser("TaRO YAMADA", 25);
+
+// console.log(uhyo.rank);
+// console.log(uhyo.name);
+// console.log(uhyo.isAdult());
+
+// function getMessage(u: User) {
+//   return `こんにちは${u.name}さん`;
+// }
+
+// const john = new User("John Smith", 15);
+// const uhyo2 = new User("uhyo", 26);
+
+// console.log(getMessage(john));
+// console.log(getMessage(uhyo2));
+// console.log(taro.isAdult());
+
+// class User {
+//   name: string;
+//   #age: number;
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.#age = age;
+//   }
+
+//   public isAdult(): boolean {
+//     return this.#age >= 20;
+//   }
+// }
+// class PremiumUser extends User {
+//   rank: number = 1;
+//   public override isAdult(): boolean {
+//     return true;
+//   }
+// }
+
+type HasName = {
+  name: string;
+};
+
+class User implements HasName {
   name: string;
   #age: number;
   constructor(name: string, age: number) {
     this.name = name;
     this.#age = age;
   }
+
   public isAdult(): boolean {
     return this.#age >= 20;
   }
 }
-class PremiumUser extends User {
-  rank: number = 1;
-  public isAdult(): boolean {
-    return true;
-  }
-}
-const uhyo = new PremiumUser("uhyo", 26);
-const taro = new PremiumUser("TaRO YAMADA", 25);
-
-console.log(uhyo.rank);
-console.log(uhyo.name);
-console.log(uhyo.isAdult());
-
-function getMessage(u: User) {
-  return `こんにちは${u.name}さん`;
-}
-
-const john = new User("John Smith", 15);
-const uhyo2 = new User("uhyo", 26);
-
-console.log(getMessage(john));
-console.log(getMessage(uhyo2));
-console.log(taro.isAdult());
