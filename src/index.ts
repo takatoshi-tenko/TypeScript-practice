39effc30fcc71b3c64e340d8e59bd8d2e02da2ca
takatoshi.tenko <chanten811@gmail.com>
2023-08-12 12:31:09 +0900

    p209 5.3.1 継承：子は親の機能を受け継ぐ

--- src/index.ts
+++ src/index.ts
@@ -870,15 +870,44 @@ import { stringify } from "querystring";
 // console.log(uhyo)
 // console.log(john)
 
+// class User {
+//   name: string = "";
+//   age: number = 0;
+// }
+// const uhyo = new User();
+// console.log(uhyo instanceof User);
+// console.log({} instanceof User);
+// const john: User = {
+//   name: "John Smith",
+//   age: 15,
+// };
+// console.log(john instanceof User);
+
 class User {
-  name: string = "";
-  age: number = 0;
+  name: string;
+  #age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.#age = age;
+  }
+  public isAdult(): boolean {
+    return this.#age >= 20;
+  }
 }
-const uhyo = new User();
-console.log(uhyo instanceof User);
-console.log({} instanceof User);
-const john: User = {
-  name: "John Smith",
-  age: 15,
-};
-console.log(john instanceof User);
+class PremiumUser extends User {
+  rank: number = 1;
+}
+const uhyo = new PremiumUser("uhyo", 26);
+console.log(uhyo.rank);
+console.log(uhyo.name);
+console.log(uhyo.isAdult());
+
+function getMessage(u: User) {
+  return `こんにちは${u.name}さん`;
+}
+
+const john = new User("John Smith", 15);
+const uhyo2 = new User("uhyo", 26);
+
+console.log(getMessage(john));
+console.log(getMessage(uhyo2));
