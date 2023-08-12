f25427d9adb2c272cc248a9469c082584fe26725
takatoshi.tenko <chanten811@gmail.com>
2023-08-12 16:31:46 +0900

    p225 5.4.3 thisを操作するメソッド

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // const message: string = "Hello World!";
 // console.log(message);
 
+import { publicDecrypt } from "crypto";
 import { stringify } from "querystring";
 import { runInContext } from "vm";
 
@@ -938,11 +939,45 @@ import { runInContext } from "vm";
 //   }
 // }
 
-type HasName = {
-  name: string;
-};
+// type HasName = {
+//   name: string;
+// };
 
-class User implements HasName {
+// class User implements HasName {
+//   name: string;
+//   #age: number;
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.#age = age;
+//   }
+
+//   public isAdult(): boolean {
+//     return this.#age >= 20;
+//   }
+
+//   public filterOlder(users: readonly User[]): User[] {
+//     return users.filter((u) => u.#age > this.#age);
+//   }
+// }
+// const uhyo = new User("uhyo", 25);
+// const john = new User("John", 15);
+// const bob = new User("Bob", 40);
+// const older = uhyo.filterOlder([john, bob]);
+// console.log(older);
+// console.log(uhyo.isAdult === john.isAdult);
+
+// const user = {
+//   name: "uhyo",
+//   age: 26,
+//   isAdult() {
+//     return this.age >= 20;
+//   },
+// };
+// console.log(user.isAdult());
+// user.age = 15;
+// console.log(user.isAdult());
+
+class User {
   name: string;
   #age: number;
   constructor(name: string, age: number) {
@@ -953,25 +988,12 @@ class User implements HasName {
   public isAdult(): boolean {
     return this.#age >= 20;
   }
-
-  public filterOlder(users: readonly User[]): User[] {
-    return users.filter((u) => u.#age > this.#age);
-  }
 }
 const uhyo = new User("uhyo", 25);
-const john = new User("John", 15);
-const bob = new User("Bob", 40);
-const older = uhyo.filterOlder([john, bob]);
-console.log(older);
-console.log(uhyo.isAdult === john.isAdult);
+const john = new User("John Smith", 15);
+console.log(uhyo.isAdult());
+console.log(uhyo.isAdult.apply(john, []));
 
-const user = {
-  name: "uhyo",
-  age: 26,
-  isAdult() {
-    return this.age >= 20;
-  },
-};
-console.log(user.isAdult());
-user.age = 15;
-console.log(user.isAdult());
+const boundIsAdult = uhyo.isAdult.bind(uhyo);
+console.log(boundIsAdult());
+console.log(boundIsAdult.call(john));
