d6a3da36ecdadb28e360fce990a5453f658b4adc
takatoshi.tenko <chanten811@gmail.com>
2023-08-12 17:39:31 +0900

    p251 6.1.2 伝搬するユニオン型

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,12 @@
 // console.log(message);
 const tama = {
     species: "Felis silvestris catus",
+    age: "永遠の17歳",
 };
 const uhyo = {
     name: "uhyo",
+    age: 26,
 };
+console.log(tama);
+console.log(uhyo);
 export {};
--- src/index.ts
+++ src/index.ts
@@ -1087,14 +1087,20 @@ import { runInContext } from "vm";
 
 type Animal = {
   species: string;
+  age: string;
 };
 type Human = {
   name: string;
+  age: number;
 };
 type User = Animal | Human;
 const tama: User = {
   species: "Felis silvestris catus",
+  age: "永遠の17歳",
 };
 const uhyo: User = {
   name: "uhyo",
+  age: 26,
 };
+console.log(tama)
+console.log(uhyo)
