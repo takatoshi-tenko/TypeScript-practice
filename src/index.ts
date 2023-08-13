3fa382527f4d64303163120a264111896ddacbe2
takatoshi.tenko <chanten811@gmail.com>
2023-08-13 16:50:33 +0900

    p358 8.3.4 Promiseオブジェクトの作成

--- src/index.ts
+++ src/index.ts
@@ -1385,16 +1385,34 @@ import { runInContext } from "vm";
 
 import { readFile } from "fs/promises";
 
-const p = readFile("foo.txt", "utf8");
-p.then((result) => {
-  console.log("成功", result);
+// const p = readFile("foo.txt", "utf8");
+// p.then((result) => {
+//   console.log("成功", result);
+// });
+// p.catch((result) => {
+//   console.log("失敗", result);
+// });
+// p.then((result) => {
+//   console.log("2");
+// });
+// p.then((result) => {
+//   console.log("3");
+// });
+
+const p = new Promise<number>((resolve) => {
+  setTimeout(() => {
+    resolve(100);
+  }, 3000);
 });
-p.catch((result) => {
-  console.log("失敗", result);
+p.then((num) => {
+  console.log(`結果は ${num}`);
 });
-p.then((result) => {
-  console.log("2");
-});
-p.then((result) => {
-  console.log("3");
+
+const sloopReject = (duration: number) => {
+  return new Promise<never>((resolve, reject) => {
+    setTimeout(reject, duration);
+  });
+};
+sloopReject(3000).catch(() => {
+  console.log("失敗!!!");
 });
